aec119b3839f49be0477ad36dc34bf644ed3262b
Manh Linh <mlinhalone@gmail.com>
2025-08-31 16:40:55 +0700

    init

---
+++ src/chapter4/Video23.java
@@ -0,0 +1,12 @@
+package chapter4;
+
+public class Video23 {
+	public static void main(String[] args) {
+
+		Student ManhLinh = new Student();
+		ManhLinh.name = "Manh Linh";
+		ManhLinh.age = 20;
+
+		System.out.printf("Student with name %s and age %d", ManhLinh.name, ManhLinh.age);
+	}
+}
